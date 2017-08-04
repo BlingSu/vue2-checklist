2fbd119553b402b16d6b7a31e07d184e13b97740
angelasubi <21978301@qq.com>
2017-08-04 14:15:04 +0800

    删除不必要都东西

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -8,8 +7,12 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: resolve => require(['../components/Home.vue'], resolve)
     }
   ]
 })
