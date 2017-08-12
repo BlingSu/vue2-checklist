391bcf57932044c5c2338413343c9f595a7c719a
angelasubi <21978301@qq.com>
2017-08-12 22:51:11 +0800

    测试bookdetail

---
+++ src/components/BookDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="book-detail">
+    <Loading v-show="loading"></Loading>
+    <div class="detail-content">
+      <div class="detail-linear">
+        测试
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Loading from './Loading/Loading.vue'
+
+export default {
+  components: {
+    Loading
+  },
+  data () {
+    return {
+      loading: false
+    }
+  }
+}
+</script>
+
+<style type="text/less" lang="less">
+
+</style>
