f85764cdf0d0136e98dfd8da395dfd1a377e3c9c
angelasubi <21978301@qq.com>
2017-09-25 14:55:35 +0800

    feat: import topnav from reader

--- src/components/Reader.vue
+++ src/components/Reader.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="reader">
-
+    <top-nav></top-nav>
   </div>
 </template>
+
+<script>
+import TopNav from './TopNav/TopNav'
+
+export default {
+  data () {
+    return {}
+  },
+  components: {
+    TopNav
+  }
+}
+</script>
