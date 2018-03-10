f784303f047532a9b9c3393b07f56cecfa713ebb
angelasubi <21978301@qq.com>
2018-03-10 17:15:19 +0800

    fix: router

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,9 @@ Vue.use(Router)
 export default new Router({
   routes: [
     { path: '/', redirect: '/recommend' },
-    { path: '/rank', component: () => import('./components/rank/rank.vue') },
-    { path: '/recommend', component: () => import('./components/recommend/recommend.vue') },
-    { path: '/search', component: () => import('./components/search/search.vue') },
-    { path: '/singer', component: () => import('./components/singer/singer.vue') }
+    { path: '/rank', component: () => import('../components/rank/rank.vue') },
+    { path: '/recommend', component: () => import('../components/recommend/recommend.vue') },
+    { path: '/search', component: () => import('../components/search/search.vue') },
+    { path: '/singer', component: () => import('../components/singer/singer.vue') }
   ]
 })
